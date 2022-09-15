1b934df4421ac75d5941f5eb26d22854e657ef57
leonardo-beltran-1019 <joseph.beltran@misena.edu.co>
2022-09-14 20:07:49 -0500

    Update productlist.php

--- TRIMESTRE V/Plantilla/productlist.php
+++ TRIMESTRE V/Plantilla/productlist.php
@@ -123,7 +123,7 @@ if(isset($_POST['buscar'])){
 					<i class="fas fa-boxes fa-fw"></i> &nbsp; LISTA DE PRODUCTOS
 				</h3>
 				<p class="text-justify">
-					Listado y estado de productos para su actualizacion.
+					Listado impresion y estado de productos para su actualizacion.
 				</p>
 			</div>
 			<div class="text-center">
