84ed6256fca68e64942052e8bbcd886bfb2bfa69
leonardo-beltran-1019 <joseph.beltran@misena.edu.co>
2022-09-14 21:10:17 -0500

    Update productlist.php
    se agrega boton imprimir

--- TRIMESTRE V/Plantilla/productlist.php
+++ TRIMESTRE V/Plantilla/productlist.php
@@ -221,8 +221,7 @@ if(isset($_POST['buscar'])){
 	  									<i class="far fa-trash-alt"></i>
 									</button>
 								</td>
-
-								
+					
 								<td>
 									<label for="estado">CAMBIO_ESTADO</label>
 									<select name="estado" id="estado">
@@ -234,6 +233,12 @@ if(isset($_POST['buscar'])){
 										<option value="ESTAMPADO">ESTAMPADO</option>
 									</select>
 									<button type="submit" value="ACTUALIZAR">Actualizar</button>
+								</td>
+								<td>
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
+  <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z"/>
+  <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+</svg>
 								</td>
 							</tr>
 							<?php
