30cbfcfccfe365e5c62455c8faf55850081bbc92
milenacastro353 <ymcastro057@misena.edu.co>
2022-09-20 21:28:39 -0500

    Se agrega busqueda por documento

--- TRIMESTRE V/Plantilla/product.php
+++ TRIMESTRE V/Plantilla/product.php
@@ -1,10 +1,24 @@
 <?php
 Include("./Conexion/conectar.php");
 
+if($_POST){
+	$Documento = $_POST['Id'];
+}
 $cone = new Conexion();
 $c = $cone->conectando();
-$query = "select * from tipos_documentos";
+$query = "select * from clientes";
 $ejecuta = mysqli_query($c,$query);
+
+if(isset($_POST['buscar'])){
+	
+    $query2="select * from clientes where Numero_Documento like '%$Documento%'";
+    $ejecuta2 = mysqli_query($c,$query2);
+    $arreglo2 = mysqli_fetch_array($ejecuta2);
+}else{
+	$query2="select * from clientes limit";
+	$ejecuta2 = mysqli_query($c,$query2);
+	$arreglo2 = mysqli_fetch_array($ejecuta2);
+}
 ?>
 
 <!DOCTYPE html>
@@ -144,7 +158,7 @@ $ejecuta = mysqli_query($c,$query);
                                                         </button>
                                                     </div>
                                                     <div class="col-6">       
-                                                        <input type="text" class="form-control" id="name" placeholder="Nombre y apellidos">   
+                                                        <input type="text" class="form-control" id="name" placeholder="Nombre y apellidos" name="<?php echo $arreglo2[0] ?>">   
                                                     </div>
                                                 </div>
                     
@@ -184,7 +198,7 @@ $ejecuta = mysqli_query($c,$query);
                                             </div>
                     
                                         <div class="aling-buttons">
-                                            <button type="submit" id="addClient" class="buttonColors btn btn-md" >
+                                            <button type="submit" id="buscar" class="buttonColors btn btn-md" >
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
                                                 <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                                                 <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
