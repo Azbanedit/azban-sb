e6f5fa821c9024dc8faeba7b62b7d59ecad4e527
milenacastro353 <ymcastro057@misena.edu.co>
2022-10-20 21:25:48 -0500

    Create Inventario.php

---
+++ TRIMESTRE V/Plantilla/Inventario.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventario</title>
+
+
+</head>
+<body>
+
+<table border="1" >
+
+<tr>
+    <th>PRODUCTO</th>
+
+    <Th>PRODUCCION</Th>
+    <th>VENTA</th>
+    <th>DISPONIBLE</th>
+</tr>
+
+<tr>
+    
+    <td align="center">Buzo capota con bolsillo</td>
+    <td align="center">30</td>
+    <td align="center">15</td>
+    <td align="center">15</td>
+
+</tr>
+
+<tr>
+    <td align="center">Buzo capota redondo con bolsillo</td>
+    <td align="center">50</td>
+    <td align="center">30</td>
+    <td align="center">20</td>
+</tr>
+
+<tr>
+    <td align="center">Buso capotero sin estampado</td>
+    <td align="center">70</td>
+    <td align="center">30</td>
+    <td align="center">40</td>
+</tr>
+
+<tr>
+    <td align="center">Buso capota redondo sin bolcillo</td>
+    <td align="center">100</td>
+    <td align="center">85</td>
+    <td align="center">15</td>
+</tr>
+
+<tr>
+    <td align="center">jogger</td>
+    <td align="center">80</td>
+    <td align="center">56</td>
+    <td align="center">24</td>
+</tr>
+</table>
+    
+</body>
+</html>
